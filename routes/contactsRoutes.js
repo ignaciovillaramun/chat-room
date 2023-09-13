0dcd44562bcd425c14f6d2182d8080d60bd64352
Oculto15 <ignaciovillar.a@gmail.com>
2023-09-13 10:47:30 -0600

    chnages

--- routes/contactsRoutes.js
+++ routes/contactsRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const meController = require("../controllers/contactsController");
 
-router.get("/all", meController.getAll);
-router.get("id/:id", meController.getOne);
+router.get("/", meController.getAll);
+router.get("/:id", meController.getOne);
 
 module.exports = router;
