1d629830317e6fcaeae50f73ad482a794e335155
Oculto15 <ignaciovillar.a@gmail.com>
2023-09-13 10:43:19 -0600

    add stuff

--- routes/contactsRoutes.js
+++ routes/contactsRoutes.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const meController = require("../controllers/contactsController");
 
 router.get("/all", meController.getAll);
-router.get("/:id", meController.getOne);
+router.get("id/:id", meController.getOne);
 
 module.exports = router;
