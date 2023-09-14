3e271cbb73330fee4d71c8a9cbb03e008393478d
Oculto15 <ignaciovillar.a@gmail.com>
2023-09-14 08:11:28 -0600

    updates

--- models/contactModels.js
+++ models/contactModels.js
@@ -1,27 +1,32 @@
 const mongoose = require("mongoose");
 
-const contactSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
-    required: true,
+const contactSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    favoriteColor: {
+      type: String,
+      required: true,
+    },
+    birthday: {
+      type: String,
+      required: true,
+    },
   },
-  lastName: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-  },
-  favoriteColor: {
-    type: String,
-    required: true,
-  },
-  birthday: {
-    type: String,
-    required: true,
-  },
-});
+  {
+    versionKey: false,
+  }
+);
 
 const Contact = mongoose.model("Contact", contactSchema);
 
