5bbd4231d9fd77d5df5a5a71a16362647a822bfb
Cricket999 <danieltbeatty9@gmail.com>
2023-11-18 00:06:08 -0700

    Render fixes

--- swagger.js
+++ swagger.js
@@ -5,13 +5,20 @@ const doc = {
     title: 'Chatroom API',
     description: 'Chatroom API',
   },
-  host: "localhost:8080",
-  schemes: ["http"],
+  // Host for local testing
+  // host: "localhost:8080",
+  // Host for deployment
+  host: "chat-room-f53d.onrender.com",
+
+  schemes: ["https"],
 
   securityDefinitions: {
     oauth2: {
       type: 'oauth2',
-      authorizationUrl: 'http://localhost:8080/auth/login',
+      // Route for local testing
+      // authorizationUrl: 'http://localhost:8080/auth/login',
+      // Route for deployment
+      authorizationUrl: 'https://chat-room-f53d.onrender.com/auth/login',
       flow: 'accessCode',
     },
   },
