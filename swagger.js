47ccbb3c33c5fc1f6e04d88c3184275276d42f8d
Oculto15 <ignaciovillar.a@gmail.com>
2023-12-02 21:48:09 -0700

    update

--- swagger.js
+++ swagger.js
@@ -1,30 +1,23 @@
 const swaggerAutogen = require('swagger-autogen')();
 
-const baseSwaggerDocument = {
+const doc = {
   info: {
     title: 'Chatroom API',
     description: 'Chatroom API',
   },
   host: 'chat-room-f53d.onrender.com',
   schemes: ['https'],
+
   securityDefinitions: {
     oauth2: {
       type: 'oauth2',
-      authorizationUrl: '{{authorizationUrl}}',
+      authorizationUrl: 'https://chat-room-f53d.onrender.com/auth/login',
       flow: 'accessCode',
     },
   },
 };
 
-const authorizationUrl =
-  process.env.NODE_ENV === 'production'
-    ? 'https://chat-room-f53d.onrender.com/auth/login'
-    : 'http://localhost:8080/auth/login';
-
-const swaggerDocument = { ...baseSwaggerDocument };
-swaggerDocument.securityDefinitions.oauth2.authorizationUrl = authorizationUrl;
-
 const outputFile = './swagger.json';
 const endpointsFiles = ['./routes/index.js'];
 
-swaggerAutogen(outputFile, endpointsFiles, swaggerDocument);
+swaggerAutogen(outputFile, endpointsFiles, doc);
