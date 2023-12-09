3b561918716b1824b2a48f5223b9bebcdd89e916
Oculto15 <ignaciovillar.a@gmail.com>
2023-12-08 19:15:54 -0700

    auth update

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,11 +1,6 @@
 exports.ensureAuthenticated = (req, res, next) => {
-  console.log('auth', process.env.NODE_ENV);
-
-  if (process.env.NODE_ENV === 'production') {
-    return res.redirect('/login');
-  }
   if (req.isAuthenticated()) {
     return next();
   }
-  return next();
+  res.redirect('/login');
 };
